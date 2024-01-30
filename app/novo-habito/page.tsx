612afad3f19f427eea6210b3948303d682490e8b
Webster Dev <webster.rafael2014@gmail.com>
2024-01-30 09:21:29 -0400

    New

--- app/novo-habito/page.tsx
+++ app/novo-habito/page.tsx
@@ -16,7 +16,7 @@ function NewHabit() {
   return (
     <main className="container relative flex flex-col gap-8 px-12 pt-16">
       <h1 className="text-4xl font-light text-center text-white font-display">
-        novo hábito
+        Nova tarefa
       </h1>
 
       <form action={newHabit} className="flex flex-col gap-4 mt-4">
@@ -31,7 +31,7 @@ function NewHabit() {
           type="submit"
           className="bg-[#45EDAD] font-display text-neutral-900 font-regular text-2xl p-2 rounded-md mt-8"
         >
-          cadastrar
+          Cadastrar
         </button>
 
         <button className="bg-neutral-800 text-[#F85858] font-display font-regular text-2xl p-2 rounded-md">
